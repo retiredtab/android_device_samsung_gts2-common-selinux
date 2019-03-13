f304188e84675d3caf3db6fcdbf0599da94db6ef
ripee <ripee.dev@gmail.com>
2019-03-13 20:06:11 +0100

    Revise TARGET_ARCH_VARIANT to armv8-a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,7 +11,7 @@ BOARD_VENDOR := samsung
 # CPU
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_VARIANT := cortex-a53.a57
 
 # Platform
