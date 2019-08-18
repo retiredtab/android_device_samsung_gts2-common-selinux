dd8f5e7b17f0ace96258c7678dc120efc3f2926e
ripee <ripee.dev@gmail.com>
2019-08-18 20:23:03 +0200

    Revert "Add custom wifi service script"
    This reverts commit 2590feb878249eec1bd4ea0c398b2be819806929.

--- device-common.mk
+++ device-common.mk
@@ -293,8 +293,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/rootdir/vendor/etc/init/android.hardware.wifi@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.wifi@1.0-service.rc
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 # Vendor
 $(call inherit-product, vendor/samsung/gts2-common/gts2-common-vendor.mk)
