710913b3b130b34175f3b7b34365c7f266efe68b
ananjaser1211 <ananjaser@gmail.com>
2019-10-07 10:41:36 +0400

    trelte-common: update cpu variant

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,7 +12,7 @@ BOARD_VENDOR := samsung
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_VARIANT := cortex-a53.a57
+TARGET_CPU_VARIANT := cortex-a53
 
 # Platform
 TARGET_BOARD_PLATFORM := exynos5
@@ -55,7 +55,7 @@ LZMA_RAMDISK_TARGETS := boot,recovery
 
 BOARD_ROOT_EXTRA_FOLDERS += efs
 TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
-	
+
 BUILD_BROKEN_DUP_RULES := true
 
 ### KERNEL
