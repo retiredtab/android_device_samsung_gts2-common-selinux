b53bf4d89c49118b6438c9657d47f9ea7b27cc6c
Arne Coucheron <arco68@gmail.com>
2020-11-14 01:03:20 +0400

    trelte-common: sensors: Replace LOCAL_PROPRIETARY_MODULE with  LOCAL_VENDOR_MODULE

--- shims/libui/Android.mk
+++ shims/libui/Android.mk
@@ -17,6 +17,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_CFLAGS := -Wall -Werror -DLOG_TAG=\"MultiHal\"
+
 LOCAL_SRC_FILES := \
     GraphicBufferMapper.cpp
 
