d1ff64741650ae894fd809106d696fe73dfdb9f3
Anan Jaser <ananjaser@gmail.com>
2020-11-14 01:26:34 +0400

    trelte-common: switch to using new HIDL power HAL

--- device-common.mk
+++ device-common.mk
@@ -226,11 +226,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.universal5433
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-service.exynos
 
 # Ramdisk
 PRODUCT_PACKAGES += \
