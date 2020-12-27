95e6e6accb3c1fe98dd4022ebacbb529b8e8257d
ripee <ripee.dev@gmail.com>
2020-12-27 14:51:36 +0100

    gts2-common: Uprev keymaster HAL to 4.0
    * this fixes formatting SD card as internal

--- device-common.mk
+++ device-common.mk
@@ -157,11 +157,9 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-   keystore.exynos5
-
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service \
+    keystore.exynos5
 
 # Lights
 PRODUCT_PACKAGES += \
