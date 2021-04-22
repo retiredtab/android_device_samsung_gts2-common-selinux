e4eaf05f7650c14d1a610d55bdd7780f4f3c4997
Anan Jaser <ananjaser@gmail.com>
2021-04-22 23:19:28 +0400

    gts2-common: build WITH_DEXPREOPT

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -100,9 +100,13 @@ BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
 
-# dex pre-optimizations
-#WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
-#WITH_DEXPREOPT := false
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= false
+    WITH_DEXPREOPT := true
+  endif
+endif
 
 # FIMG2D
 BOARD_USES_SKIA_FIMGAPI := true
