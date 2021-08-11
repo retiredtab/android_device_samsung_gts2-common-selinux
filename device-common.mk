4b7dd3bd17a4b2586c3648edd0cbd324eefc4b3d
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-08-11 10:24:52 -0600

    gts2-common: use stock bluetooth passthrough implementation

--- device-common.mk
+++ device-common.mk
@@ -40,7 +40,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl-gts2 \
+    android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
