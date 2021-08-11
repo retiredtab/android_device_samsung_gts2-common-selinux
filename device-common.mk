7b83a07403a6d12a26c4f1a1115fd9ad48cc5f4e
dianlujitao <dianlujitao@lineageos.org>
2021-08-11 10:47:38 -0600

    gts2-common: Add prebuilt protobuf from sdk29
    This is needed for our prebuilt DRM libraries.

--- device-common.mk
+++ device-common.mk
@@ -271,6 +271,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# VNDK
+# Patch/hexedit DRM to look for older version of libprotobuf-cpp-lite.so
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-v29-lite.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service.legacy \
