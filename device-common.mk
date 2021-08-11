a87e65a64a1ff7e03c347179b4fa24fb1628de45
Bruno Martins <bgcngm@gmail.com>
2021-08-11 10:36:14 -0600

    gts2-common: Move graphics composer to hwbinder
    The passthrough impl is dead as of Android 11 QPR1.

--- device-common.mk
+++ device-common.mk
@@ -124,7 +124,7 @@ PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     libfimg \
     libhwc2on1adapter \
