17cede4f27f0389a9d9a6601206b291836ead6ca
retiredtab-gerrit <you@example.com>
2022-03-26 20:22:44 -0600

    device-common: uprev audio to 6.0

--- device-common.mk
+++ device-common.mk
@@ -13,8 +13,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     audio.a2dp.default \
     audio.primary.universal5433 \
     audio.r_submix.default \
