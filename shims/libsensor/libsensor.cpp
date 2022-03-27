77cf1eb9775b608ef3c95a93954c39fa1af561ae
Elim Garak <elim.garak.dev@gmail.com>
2022-03-26 20:45:26 -0600

    device-common: fix for missing symbol "_ZN7android13SensorManager16ceateEventQueueENS_7String8Ei"
    12-11 20:34:22.637  3028  3028 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gpsd": cannot locate symbol "_ZN7android13SensorManager16createEventQueueENS_7String8Ei" referenced by "/system/vendor/bin/gpsd"...

--- shims/libsensor/libsensor.cpp
+++ shims/libsensor/libsensor.cpp
@@ -21,6 +21,13 @@
 
 #include "libsensor.h"
 
+#include <stdlib.h>
+// android::SensorManager::getSensorList(android::Sensor const* const**) const
+extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE();
+extern "C" void _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(){};
+extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei();
+extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei(){}
+
 namespace android
 {
 
