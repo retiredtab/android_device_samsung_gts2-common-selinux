44cf5d54af1b5014a262b2be88ef52221ef7e1b7
Tim Zimmermann <tim@linux4.de>
2022-04-24 12:19:01 -0600

    gts2-common:  Switch to AIDL libperfmgr power HAL

--- device-common.mk
+++ device-common.mk
@@ -228,7 +228,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.universal5433
+    android.hardware.power-service.samsung-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -258,6 +261,13 @@ PRODUCT_PACKAGES += \
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/samsung/aidl/power-libperfmgr
 
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
