be45aa0d0b7c29550e220cb41acda2a6824ac2e9
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-12-02 16:01:01 -0700

    gts2-common: add missing symbols for gps shim

--- shims/libsensor/libsensor.cpp
+++ shims/libsensor/libsensor.cpp
@@ -21,4 +21,22 @@ namespace android {
         _ZN7android8String16D1Ev(&string);
     }
 
+    extern "C" void _ZN7android8String16aSEOS0_();
+    extern "C" void _ZN7android8String16aSEOS0_(){}
+
+    extern "C" void _ZN7android8String16C1EOS0_();
+    extern "C" void _ZN7android8String16C1EOS0_(){}
+
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE();
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE(){}
+
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE();
+    extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE(){}
+
+    extern "C" void _ZN7android13SensorManager16createEventQueueEv();
+    extern "C" void _ZN7android13SensorManager16createEventQueueEv(){}
+
+    extern "C" void _ZN7android13SensorManagerC1Ev();
+    extern "C" void _ZN7android13SensorManagerC1Ev(){}
+
 }
