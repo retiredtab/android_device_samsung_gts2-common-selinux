e1d02238470fe3c7bf793783c69a40c5b27e6369
Anan Jaser <ananjaser@gmail.com>
2023-07-16 17:42:05 -0600

    gts2-common: switch to new GPU and omx drivers
    Needed to support R22P0 GPU drivers and kanged omx drivers.
    
    See
    
    https://github.com/universal5433/proprietary_vendor_samsung/commit/c105d6037c7ad74a030101a946a53a2faf587e3d
    
    https://github.com/universal5433/proprietary_vendor_samsung/commit/c26d77cdbc30eb9301e34b151b170666a2bb861c

--- device-common.mk
+++ device-common.mk
@@ -66,7 +66,7 @@ PRODUCT_PACKAGES += \
 
 # Codecs
 PRODUCT_PACKAGES += \
-    libExynosOMX_Core \
+    libion_exynos \
     libstagefrighthw
 
 # Configstore
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     libandroid_net \
     netutils-wrapper-1.0
+
+# OMX
+PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
